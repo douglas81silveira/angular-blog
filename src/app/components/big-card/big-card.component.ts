21e6bc49cbb23bd676ead332ee203c4378426bba
Douglas <douglas81silveira@gmail.com>
2023-04-07 01:15:07 -0300

    Inclusão de propriedades nos components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string =
+    'https://stonegatesl.com/wp-content/uploads/2021/04/placeholder.jpg';
+  @Input()
+  cardTitle: string = 'Card Title';
+  @Input()
+  cardDescription: string = 'Card Description.';
 
+  constructor() {}
 }
